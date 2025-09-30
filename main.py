0454be00b81ecd998b2d6edf4b37f42ac3b4fcb7
nitipoommorndee-wq <nitipoommorndee@gmail.com>
2025-09-30 18:46:14 +0700

    main.py32

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def ask(prompt, default=None):
     return default if s == "" and default is not None else s
 
 def main():
-    # ถาม input ตัวอักษร
+    
     char = ask("Enter a character (default = x): ", default="x")
     
-    # ถาม input จำนวนรอบ
+    
     turns_raw = ask("How many turns you want to run [4]: ", default="4")
 
     try:
@@ -23,7 +23,7 @@ def main():
         print("Turns must be non-negative. Using 0.")
         turns = 0
 
-    # แสดงผลลัพธ์
+    
     for i in range(1, turns + 1):
         print(myfunc(char, i))
 
